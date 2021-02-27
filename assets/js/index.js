4a0ae87f5edeb845147edb6c02dcbd8007f20e3d
RaidenMT <raikun96@gmail.com>
2021-02-27 18:10:24 -0400

    Promotions carousel added

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+new Splide('#splide', {
+    direction: 'ttb',
+    width: '100vw',
+    height: '100vh',
+    type: 'loop',
+    autoplay: true,
+    arrowPath: 'm15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z',
+}).mount();
+
